207bcb6f251009cb9dcbcc349625d7fb6c48571b
amit.naikwadi84@gmail.com <amit.naikwadi84@gmail.com>
2018-08-25 14:50:37 +0530

    Initial commit for service for Hystrix

---
+++ src/main/java/com/amit/springcloud/weatherservice/WeatherServiceApplication.java
@@ -0,0 +1,28 @@
+package com.amit.springcloud.weatherservice;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class WeatherServiceApplication {
+	
+	private String[] weather = new String[] {"sunny","cloudy","windy","rainy"};
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServiceApplication.class, args);
+	}
+	
+	
+	@GetMapping("/weather")
+	public String getWeather() {
+		int random=ThreadLocalRandom.current().nextInt(0,4);
+		return weather[random];
+	}
+}
